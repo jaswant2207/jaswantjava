7b440a21167f98cb80ce8842d7d2a0d34084c741
Jaswant <jaswantu2207@gmail.com>
2025-09-18 11:56:04 +0530

    Create logo.java

---
+++ logo.java
@@ -0,0 +1,11 @@
+public class logo{
+    public static void main(String[]args){
+        int i=1;
+       int n=10;
+        while(i<=n){
+            System.out.print(i);
+            i++;
+            
+        }
+}
+}
