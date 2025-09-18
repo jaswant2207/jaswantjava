f341243dd144a66972ae354ead9072f859f473ea
Jaswant <jaswantu2207@gmail.com>
2025-09-18 11:57:22 +0530

    Create Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,11 @@
+class Arithmetic {
+    public static void main(String[]args){
+        int a =10;
+        int b =5;
+        System.out.println("a+b"+"="+(a+b));
+        System.out.println("a+b"+"="+(a-b));
+        System.out.println("a*b"+"="+(a*b));
+        System.out.println("a/b"+"="+(a/b));
+        System.out.println("a%b"+"="+(a%b));
+}        
+    }
