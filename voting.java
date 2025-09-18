057daeeb1b20b8152c3b105889bebf0957078af2
Jaswant <jaswantu2207@gmail.com>
2025-09-18 11:52:40 +0530

    Create voting.java

---
+++ voting.java
@@ -0,0 +1,11 @@
+class voting {
+    public static void main(String[] args) {
+        int a = 20;
+        if (a>=18){
+            System.err.println("ELIGIBLE FOR VOTING");
+        }
+   else{
+    System.err.println("NOT ELIGIBLE FOR VOTING ");
+   }
+    }
+}
