ae9b24542184bd414babe068abef0c8a0eb926b4
Jaswant <jaswantu2207@gmail.com>
2025-09-18 11:54:46 +0530

    Create declare.java

---
+++ declare.java
@@ -0,0 +1,14 @@
+class declare{
+    public static void main(String args[]){
+        int a = 0;
+        if(a>0){
+            System.out.println("It is positive");
+        }
+        else if(a<0){
+            System.out.println("It is negative");
+        }
+        else if(a==0){
+            System.out.println("A number is 0");
+        }
+    }
+}  
